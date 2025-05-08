112a65b2982cff99079e6ff9d79cd8ac3a9fb4e5
ArielBorbon <ariel.borbon252116@potros.itson.edu.mx>
2025-05-08 02:09:53 -0700

    Buscador Platillos
    ya busca por nombre y categoria, solo falta lo de regresar uno para ir armando el carrito (ya que lleguemos a ese frame) Y para el administrador en un futuro el esta disponible o no

---
+++ Kiawa_Proyecto/Subsistema_Empleado/src/main/java/com/mycompany/subsistema_empleado/Subsistema_Empleado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.subsistema_empleado;
+
+/**
+ *
+ * @author PC Gamer
+ */
+public class Subsistema_Empleado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Kiawa_Proyecto/Subsistema_MedioPago/src/main/java/com/mycompany/subsistema_mediopago/Subsistema_MedioPago.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.subsistema_mediopago;
+
+/**
+ *
+ * @author PC Gamer
+ */
+public class Subsistema_MedioPago {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
