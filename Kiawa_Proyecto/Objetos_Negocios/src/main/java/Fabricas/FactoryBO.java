b06fe89c9f670f4bfce2292225b3f5661e96d7e6
ArielBorbon <ariel.borbon252116@potros.itson.edu.mx>
2025-05-07 18:43:17 -0700

    FactorysBO
    una fabrica de bos ya que estos tienen como parametro el DAO y esta se usa para crear objetos negocios de cada tipo

--- Kiawa_Proyecto/Objetos_Negocios/src/main/java/Fabricas/FactoryBO.java
+++ Kiawa_Proyecto/Objetos_Negocios/src/main/java/Fabricas/FactoryBO.java
@@ -4,10 +4,58 @@
  */
 package Fabricas;
 
+import BO.AdministradorBO;
+import BO.AlumnoBO;
+import BO.CocineroBO;
+import BO.PedidoBO;
+import BO.PlatilloBO;
+import BO.RepartidorBO;
+import BO.UbicacionBO;
+import DAO.AdministradorDAO;
+import DAO.CocineroDAO;
+import DAO.PedidoDAO;
+import DAO.PlatilloDAO;
+import DAO.RepartidorDAO;
+import DAO.UbicacionDAO;
+
 /**
  *
  * @author PC Gamer
  */
 public class FactoryBO {
-    
+
+    public static PlatilloBO crearPlatilloBO() {
+        PlatilloDAO dao = new PlatilloDAO();
+        return new PlatilloBO(dao);
+    }
+
+    public static PedidoBO crearPedidoBO() {
+        PedidoDAO dao = new PedidoDAO();
+        return new PedidoBO(dao);
+    }
+
+    public static UbicacionBO crearUbicacionBO() {
+        UbicacionDAO dao = new UbicacionDAO();
+        return new UbicacionBO(dao);
+    }
+
+    public static RepartidorBO crearRepartidorBO() {
+        RepartidorDAO dao = new RepartidorDAO();
+        return new RepartidorBO(dao);
+    }
+
+    public static CocineroBO crearCocineroBO() {
+        CocineroDAO dao = new CocineroDAO();
+        return new CocineroBO(dao);
+    }
+
+    public static AdministradorBO crearAdministradorBO() {
+        AdministradorDAO dao = new AdministradorDAO();
+        return new AdministradorBO(dao);
+    }
+
+    public static AlumnoBO crearAlumnoBO() {
+        return new AlumnoBO();
+    }
+
 }
